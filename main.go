84cc210224ed7256178afd33f1bc18d47ff13dc3
Yusuke Yasuo <yasuo.y@dely.jp>
2023-04-16 21:46:34 +0900

    Enable to save new page

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ type Page struct{
 	Body []byte
 }
 
+func (p *Page) save() error{
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
 func loadPage(title string) (*Page, error){
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
@@ -28,7 +33,11 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
 
 func viewHandler(w http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil{
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
 	renderTemplate(w, "view", p)
 }
 
@@ -41,8 +50,22 @@ func editHandler(w http.ResponseWriter, r *http.Request){
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+
+}
+
 func main(){
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
